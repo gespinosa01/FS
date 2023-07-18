383915b581bca09263905d7922d6a7e9beba02a7
GadyelMtz <gajomartinezgu@ittepic.edu.mx>
2023-07-17 17:31:43 -0700

    NS pruebas añadido

---
+++ NS - Pruebas.asm
@@ -0,0 +1,318 @@
+;***********************************************
+;   Nombre del proyecto:
+;   FIRULAIS SOLOVINO
+;   
+;   Integrantes:
+;   1. Gadyel Josue Martínez Guzmán
+;   2. Gerardo Espinosa Rosas
+;   3. Jose Manuel Vazquez Mendez
+;   Presenta:
+;   
+;-------------------------------------------------------------
+;FASE 1. MAQUETADO de SOFTWARE           -Fecha 13 julio 2023-
+;------------------------------------------------------------- 
+;INSTRUCCIONES:
+;   1.  Definir una paleta de colores, máximo 3 colores
+;       cada pantalla, deberá estar alineada a la paleta
+;       de colores.
+;   2.  Por equipo, definir un logotipo. Debe ser original.
+;   3.  Cada pantalla deberá tener el logotipo, un encabezado
+;       y un pie de página.
+;   4.  Cada pantalla deberá tener un fondo de algún caracter.
+;   5.  Definir el menú, pantalla login, pantalla <1>.
+;**************************************************************
+.MODEL SMALL           
+.STACK
+.DATA
+    ;**************** VARIABLES LOGIN *************************
+    LINEA           DB  80  DUP(219)
+    MSJUSUARIO      DB  "USUARIO:       "  
+    MSJCONTRASEÑA   DB  "CONTRASE",165,"A:    "    
+    USUARIO         DB  "  FIRULAIS"
+    CONTRASEÑA      DB  "  SOLOVINO" 
+    USUARIOC        DB  9, 0, 9 DUP(36)
+    CONTRASEÑAC     DB  9, 0, 9 DUP(36)
+    USUARIOINC      DB  "Ha introducido un usuario incorrecto";36
+    USUARIOCOR      DB  "Usuario correcto                    ";36 
+    CONTRASEÑACOR   DB  "Contrase",164,"a correcta                     ";40
+    CONTRASEÑAINC   DB  "Ha introducido una contrase",164,"a incorrecta";40
+    RENGLON         DB  0
+    CONTADOR        DB  0
+    LOGOG           DB  10,13,10,13,'                    ',219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,'    ',219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,'                   ',10,13
+                    DB  '                                                                               ',10,13
+                    DB  '                    ',219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,'    ',219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,'                   ',10,13
+                    DB  '                                                                               ',10,13
+                    DB  '                    ',219,219,219,219,'                 ',219,219,219,219,'                                  ',10,13
+                    DB  '                                                                               ',10,13
+                    DB  '                    ',219,219,219,219,'                 ',219,219,219,219,'                                  ',10,13
+                    DB  '                                                                               ',10,13
+                    DB  '                    ',219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,'        ',219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,'                   ',10,13
+                    DB  '                                                                               ',10,13
+                    DB  '                    ',219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,'        ',219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,'                   ',10,13
+                    DB  '                                                                               ',10,13
+                    DB  '                    ',219,219,219,219,'                                   ',219,219,219,219,'                ',10,13
+                    DB  '                                                                               ',10,13
+                    DB  '                    ',219,219,219,219,'                                   ',219,219,219,219,'                ',10,13
+                    DB  '                                                                               ',10,13
+                    DB  '                    ',219,219,219,219,'                 ',219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,'                   ',10,13
+                    DB  '                                                                               ',10,13
+                    DB  '                                                                               ',10,13 
+;***************************** FIN VARIABLES LOGIN ********************************************
+;==============================================================================================
+;***************************** VARIABLES MENÚ *************************************************
+    MSJTITULO   DB      '     FUNCIONALIDADES     '
+    MSJFUNCION  DB      'Elije una funcion:[   ]  '
+    MSJOPCION1  DB      '1.Enviar recursos        '
+    MSJOPCION2  DB      '2.Detectar enemigos      '
+    MSJOPCION3  DB      '3.Enviar mensajes        ' 
+    OPCION      DB      0,'$'
+    ESLOGAN     DB      'FIRULAIS SOLOVINO - EL MENSAJERO DE ESPERANZA'
+    NOMBRE1     DB      'GERARDO ESPINOSA ROSAS';22   
+    NOMBRE2     DB      'GADYEL JOSUE M',160,'RTINEZ G',163,'ZMAN';28
+    NOMBRE3     DB      'JOS',130,' MANUEL V',160,'ZQUEZ M',130,'NDEZ';26
+    MATERIA     DB      'LENGUAJEZ DE INTERFAZ';21
+    MAESTRA     DB      'MA. ELENA PARRA UR',161,'AS';21
+    PROYECTO    DB      'PROYECTO FINAL';14
+    LOGOP       DB      '                                                       ',10,13,'    ',219,219,219,219,219,219,219,'    ',10,13  
+                DB      '                                                       ','  ',219,219,'       ',219,219,'  ',10,13    
+                DB      '                                                       ',' ',219,'  ',219,'     ',219,'  ',219,' ',10,13      
+                DB      '                                                       ',219,'  ',219,'       ',219,'  ',219,10,13
+                DB      '                                                       ',219,'  ',219,' ',219,'   ',219,' ',219,'  ',219,10,13 
+                DB      '                                                       ',' ',219,' ',219,'       ',219,' ',219,' ',10,13 
+                DB      '                                                       ',' ',219,' ',219,'       ',219,' ',219,' ',10,13
+                DB      '                                                       ','  ',219,' ',219,' ',219,219,219,' ',219,' ',219,'  ',10,13
+                DB      '                                                       ','    ',219,'  ',219,'  ',219,'    ',10,13
+                DB      '                                                       ','     ',219,'   ',219,'     ',10,13
+                DB      '                                                       ','      ',219,219,219,'      ',10,13
+                
+    LOGO        DB 10,13,'                    ___  ___                 ',10,13
+                DB '                    |  \/  |                 ',10,13
+                DB '                    | .  . | ___ _ __  _   _ ',10,13
+                DB '                    | |\/| |/ _ \ ´_ \| | | |',10,13
+                DB '                    | |  | |  __/ | | | |_| |',10,13
+                DB '                    \_|  |_/\___|_| |_|\__,_|',10,13         
+
+
+;==============================================================================
+;                                   MACROS                           
+;==============================================================================  
+IMP_CAD_COLOR MACRO CADENA, LONGITUD, RENGLON, COLUMNA, MODO, COLOR, PAGINA 
+    MOV AH,19
+    LEA BP,CADENA
+    MOV CX,LONGITUD
+    MOV DH,RENGLON
+    MOV DL,COLUMNA
+    MOV AL,MODO
+    MOV BL,COLOR
+    MOV BH,PAGINA
+    INT 10H    
+IMP_CAD_COLOR ENDM 
+
+CURSOR MACRO RENGLON,COLUMNA,PAGINA
+    MOV AH,2
+    MOV DH,RENGLON
+    MOV DL,COLUMNA,
+    MOV BH,PAGINA
+    INT 10H
+CURSOR ENDM 
+
+LEER_CADENA MACRO CADENA 
+    MOV AH,10
+    LEA DX,CADENA
+    INT 21H
+LEER_CADENA ENDM
+;============================================================
+;                               CODE
+;============================================================                             
+.CODE
+;***************************** LOGIN ************************
+LOGIN:
+    MOV AX, @DATA
+    MOV DS, AX
+    MOV ES, AX
+
+MOV CX,25
+FONDOLOGIN:
+    PUSH CX
+    IMP_CAD_COLOR LINEA,80,RENGLON,0,0,8FH,0
+        INT 10H        
+        INC RENGLON
+    POP CX
+LOOP FONDOLOGIN
+
+LOGO_LOGIN:
+    MOV AH,19
+    LEA BP,LOGOG
+    MOV CX,1360
+    MOV DH,0    ;renglon
+    MOV DL,0    ;columna
+    MOV AL,0    ;modo
+    MOV BH,0
+    MOV BL,0F8H
+    INT 10H
+
+USUARIO_INICIO:
+IMP_CAD_COLOR   MSJUSUARIO, 15, 20, 30, 0, 0F8H, 0
+
+CURSOR 20, 46, 0
+LEER_CADENA USUARIOC
+
+MOV CONTADOR, 0
+MOV CX, 8
+MOV SI, 2
+COMPARAR_USUARIO:
+    MOV AL, USUARIOC[SI]
+    MOV BL, USUARIO[SI]
+    CMP AL, BL
+    JE CMP_USUARIOCOR
+    JMP CMP_USUARIOINC
+    
+CMP_USUARIOCOR:
+    INC CONTADOR
+    INC SI    
+    LOOP COMPARAR_USUARIO
+    JMP FIN_COMPARAR_USUARIO
+    
+CMP_USUARIOINC:
+    INC SI
+    LOOP COMPARAR_USUARIO
+    JMP FIN_COMPARAR_USUARIO
+    
+FIN_COMPARAR_USUARIO:
+    CMP CONTADOR, 8
+    JE  USUARIOCORRECTO
+    JMP USUARIOINCORRECTO
+    
+    
+USUARIOCORRECTO: 
+    IMP_CAD_COLOR USUARIOCOR, 36, 21, 30, 0, 0F8H, 0
+    JMP CONTRASEÑA_INICIO
+
+USUARIOINCORRECTO:
+    IMP_CAD_COLOR   USUARIOINC, 36, 21, 30, 0, 0FCH, 0
+    JMP USUARIO_INICIO
+
+;**********     SI EL USUARIO FUE CORRECTO ***********
+CONTRASEÑA_INICIO:
+IMP_CAD_COLOR   MSJCONTRASEÑA, 15, 22, 30, 0, 0F8H, 0
+
+CURSOR 22, 46, 0
+
+LEER_CADENA CONTRASEÑAC
+
+MOV CX, 8
+MOV SI, 2
+MOV AX, 0
+MOV BX, 0
+MOV CONTADOR, 0
+COMPARAR_CONTRASEÑA:
+    MOV AL, CONTRASEÑAC[SI]
+    MOV BL, CONTRASEÑA[SI]
+    CMP AL, BL
+    JE CMP_CONTRASEÑACOR
+    JMP CMP_CONTRASEÑAINC
+    
+CMP_CONTRASEÑACOR:
+    INC CONTADOR
+    INC SI    
+    LOOP COMPARAR_CONTRASEÑA
+    JMP FIN_COMPARAR_CONTRASEÑA
+    
+CMP_CONTRASEÑAINC:
+    INC SI
+    LOOP COMPARAR_CONTRASEÑA
+    JMP FIN_COMPARAR_CONTRASEÑA
+    
+FIN_COMPARAR_CONTRASEÑA:
+    CMP CONTADOR, 8
+    JE  CONTRASEÑACORRECTA
+    JMP CONTRASEÑAINCORRECTA
+    
+    
+CONTRASEÑACORRECTA: 
+    IMP_CAD_COLOR CONTRASEÑACOR, 40, 23, 30, 0, 0F8H, 0
+    NOP
+    JMP MENU
+
+CONTRASEÑAINCORRECTA:
+    IMP_CAD_COLOR   CONTRASEÑAINC, 40, 23, 30, 0, 0FCH, 0
+    JMP CONTRASEÑA_INICIO
+
+;***********************    FIN LOGIN    *************************
+    
+;=================================================================
+;                            MENÚ
+;=================================================================    
+MENU:
+MOV CX,25
+MOV RENGLON, 0
+FONDOMENU:
+    PUSH CX
+    IMP_CAD_COLOR LINEA, 80, RENGLON, 0, 0, 0FCH, 0
+        INT 10H        
+        INC RENGLON
+    POP CX
+LOOP FONDOMENU
+;******************** ENCABEZADO *********************************
+    IMP_CAD_COLOR LINEA, 80, 0, 0, 0, 08H,0
+    IMP_CAD_COLOR LINEA, 80, 1, 0, 0, 08H,0
+    IMP_CAD_COLOR LINEA, 80, 2, 0, 0, 08H,0
+    
+    IMP_CAD_COLOR ESLOGAN, 45, 1, 18, 0, 8FH, 0                                         
+
+;******************** FIN ENCABEZADO *****************************
+
+
+;******************** PIE DE PAGINA  *****************************
+    IMP_CAD_COLOR LINEA, 80, 22, 0, 0, 08H, 0
+    IMP_CAD_COLOR LINEA, 80, 23, 0, 0, 08H, 0
+    IMP_CAD_COLOR LINEA, 80, 24, 0, 0, 08H, 0
+    
+    IMP_CAD_COLOR NOMBRE1,  22, 22,  3, 0, 8FH, 0
+    IMP_CAD_COLOR PROYECTO, 14, 22, 55, 0, 8FH, 0 
+    
+    IMP_CAD_COLOR NOMBRE2, 28, 23,  3, 0, 8FH, 0
+    IMP_CAD_COLOR MATERIA, 21, 23, 55, 0, 8FH, 0
+    
+    IMP_CAD_COLOR NOMBRE3, 26, 24,  3, 0, 8FH, 0
+    IMP_CAD_COLOR MAESTRA, 21, 24, 55, 0, 8FH, 0
+;******************* FIN PIE DE PAGINA ***************************
+;IMPRIMIR LOGO
+    MOV AH, 19
+    LEA BP, LOGOP
+    MOV CX, 794
+    MOV DH, 3    ;renglon
+    MOV DL, 55    ;columna
+    MOV AL, 0    ;modo
+    MOV BH, 0
+    MOV BL, 0CFH
+    INT 10H
+
+;IMPRIMIR MENU
+    MOV AH, 19
+    LEA BP, LOGO
+    MOV CX, 290
+    MOV DH, 3    
+    MOV DL, 40    
+    MOV AL, 0    
+    MOV BH, 0
+    MOV BL, 0CFH
+    INT 10H
+  
+
+FIN:
+        MOV AX, 4C00H
+        INT 21H
+END
+
+
+
+
+
+
+
+
+
+
+
+       
