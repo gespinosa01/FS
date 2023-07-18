0ccbe54e343593bf4a941c6d480ec8b65d17913a
JMVM12 <jvazquezm02@hotmail.com>
2023-07-17 18:49:43 -0700

    Menu principal

--- menu.asm
+++ menu.asm
@@ -2,11 +2,12 @@
 .STACK
 .DATA 
     msjTitulo   DB      '     FUNCIONALIDADES     '
-    msjFuncion  DB      'Elije una funcion:[   ]  '
+    msjFuncion  DB      '[PRESIONA ENTER PARA CONTINUAR]';31
     msjOpcion1  DB      '1.Enviar recursos        '
     msjOpcion2  DB      '2.Detectar enemigos      '
     msjOpcion3  DB      '3.Enviar mensajes        ' 
-    opcion      DB      0,'$'
+    opcion      DB      0,'$' 
+    rastreo     db      0
     ESLOGAN     DB      'FIRULAIS SOLOVINO - EL MENSAJERO DE ESPERANZA'
     NOMBRE1     DB      'GERARDO ESPINOSA ROSAS';22   
     NOMBRE2     DB      'GADYEL JOSUE M',160,'RTINEZ G',163,'ZMAN';28
@@ -16,24 +17,24 @@
     PROYECTO    DB      'PROYECTO FINAL';14
     RENGLON        DB 0
     linea          db 80 dup(219) ;reserva 80 bytes con el caracter 219
-    LOGOP       DB      10,13,'    ',219,219,219,219,219,219,219,'    ',10,13  
-                DB      '  ',219,219,'       ',219,219,'  ',10,13    
-                DB      ' ',219,'  ',219,'     ',219,'  ',219,' ',10,13      
-                DB      219,'  ',219,'       ',219,'  ',219,10,13
-                DB      219,'  ',219,' ',219,'   ',219,' ',219,'  ',219,10,13 
-                DB      ' ',219,' ',219,'       ',219,' ',219,' ',10,13 
-                DB      ' ',219,' ',219,'       ',219,' ',219,' ',10,13
-                DB      '  ',219,' ',219,' ',219,219,219,' ',219,' ',219,'  ',10,13
-                DB      '    ',219,'  ',219,'  ',219,'    ',10,13
-                DB      '     ',219,'   ',219,'     ',10,13
-                DB      '      ',219,219,219,'      ',10,13
+    LOGOP       DB      10,13,'                                                       ','    ',219,219,219,219,219,219,219,'    ',10,13  
+                DB      '                                                       ','  ',219,219,'       ',219,219,'  ',10,13    
+                DB      '                                                       ',' ',219,'  ',219,'     ',219,'  ',219,' ',10,13      
+                DB      '                                                       ',219,'  ',219,'       ',219,'  ',219,10,13
+                DB      '                                                       ',219,'  ',219,' ',219,'   ',219,' ',219,'  ',219,10,13 
+                DB      '                                                       ',' ',219,' ',219,'       ',219,' ',219,' ',10,13 
+                DB      '                                                       ',' ',219,' ',219,'       ',219,' ',219,' ',10,13
+                DB      '                                                       ','  ',219,' ',219,' ',219,219,219,' ',219,' ',219,'  ',10,13
+                DB      '                                                       ','    ',219,'  ',219,'  ',219,'    ',10,13
+                DB      '                                                       ','     ',219,'   ',219,'     ',10,13
+                DB      '                                                       ','      ',219,219,219,'      ',10,13
                 
     LOGO        DB 10,13,'                    ___  ___                 ',10,13
                 DB '                    |  \/  |                 ',10,13
                 DB '                    | .  . | ___ _ __  _   _ ',10,13
-                DB '                    | |\/| |/ _ \ ´_ \| | | |',10,13
+                DB '                    | |\/| |/ _ \ ?_ \| | | |',10,13
                 DB '                    | |  | |  __/ | | | |_| |',10,13
-                DB '                    \_|  |_/\___|_| |_|\__,_|',10,13         
+                DB '                    \_|  |_/\___|_| |_|\__,_|',10,13           
 ;***************SECCION DE MACROS*************
 IMP_CAD_COLOR MACRO CADENA, LONGITUD, RENGLON, COLUMNA, MODO, COLOR, PAGINA 
     MOV AH,19
@@ -98,41 +99,51 @@ LOOP FONDOPANTALLA2
     IMP_CAD_COLOR NOMBRE3,26,24,3,0,8FH,0
     IMP_CAD_COLOR MAESTRA,21,24,55,0,8FH,0
 ;==============================================================================       
-        ; paso 1 imprimir MENU
-        MOV AH,19
-        LEA BP,LOGO
-        MOV CX,290
-        MOV DH,3    ;renglon
-        MOV DL,40    ;columna
-        MOV AL,0    ;modo
-        MOV BH,0
-        MOV BL,0CFH
-        INT 10H
+        ; paso 1 imprimir MENU 
+      ;IMPRIMIR LOGO
+    MOV AH, 19
+    LEA BP, LOGOP
+    MOV CX, 794
+    MOV DH, 3    ;renglon
+    MOV DL, 55    ;columna
+    MOV AL, 0    ;modo
+    MOV BH, 0
+    MOV BL, 0CFH
+    INT 10H
+
+;IMPRIMIR MENU
+    MOV AH, 19
+    LEA BP, LOGO
+    MOV CX, 284
+    MOV DH, 3    
+    MOV DL, 40    
+    MOV AL, 0    
+    MOV BH, 0
+    MOV BL, 0CFH
+    INT 10H 
         ; paso 1 imprimir LOGO
-        MOV AH,19
-        LEA BP,LOGOP
-        MOV CX,189
-        MOV DH,3    ;renglon
-        MOV DL,0    ;columna
-        MOV AL,0    ;modo
-        MOV BH,0
-        MOV BL,0CFH
-        INT 10H  
+        
         ;imprimir menu
         IMP_CAD_COLOR msjtitulo,25,12,18,0,0CFH,0
-        IMP_CAD_COLOR msjFuncion,25,20,20,0,0CFH,0
         IMP_CAD_COLOR msjOpcion1,25,14,20,0,0CFH,0
         IMP_CAD_COLOR msjOpcion2,25,16,20,0,0CFH,0
         IMP_CAD_COLOR msjOpcion3,25,18,20,0,0CFH,0
-       MOV AH,2
-       MOV DH,20
-       MOV DL,40
-       MOV BH,0
-       INT 10H
-       ;Esperar tecla
-        MOV AH,1
-        INT 21H
-            MOV opcion,AL 
+        IMP_CAD_COLOR msjFuncion,31,20,20,0,0CFH,0
+        MOV AH,2 ;cursor
+        MOV DH,20 ;renglon
+        MOV DL,90 ;columna
+        MOV BH,0  ;pagina
+        INT 10H   
+            MOV AH,0
+            INT 16H
+            MOV rastreo,AH ;RECUPERAR 
+        
+        CMP rastreo,1CH
+        JE Ventanarecursos
+        
+Ventanarecursos:
+    JMP fin    
+        
 ;---------------------------------------------------------------------
            
 FIN: MOV AX,4C00H
